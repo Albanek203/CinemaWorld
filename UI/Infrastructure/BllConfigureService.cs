f0a90041c437697e8edd77d4d0ab7a7f1cb8c5d8
Oleh <oleglovchuk@ukr.net>
2021-12-14 17:20:52 +0100

    Added class BllConfigureService for config services

---
+++ UI/Infrastructure/BllConfigureService.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Services;
+using DLL.Context;
+using DLL.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UI.Infrastructure {
+    public static class BllConfigureService {
+        public static void ConfigureService(IServiceCollection services, string connectionStr) {
+            // DbContextOptionsBuilder
+            services.AddTransient(x => {
+                var optionBuilder = new DbContextOptionsBuilder<CinemaContext>();
+                var option =
+                        optionBuilder.UseSqlServer(connectionStr).Options;
+                return option;
+            });
+
+            // Context
+            services.AddTransient<CinemaContext>();
+
+            // Repositories
+            services.AddTransient<ActionsDataRepository>();
+            services.AddTransient<FilmRepository>();
+            services.AddTransient<LoginDataRepository>();
+            services.AddTransient<PersonRepository>();
+            services.AddTransient<SeatRepository>();
+            services.AddTransient<SessionRepository>();
+            services.AddTransient<UserRepository>();
+
+            // Services
+            services.AddTransient<LoginService>();
+            services.AddTransient<TicketService>();
+        }
+    }
+}
