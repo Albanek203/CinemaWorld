ccb4642f976f9ca209aa04b0993ee3def94ea592
Oleh <oleglovchuk@ukr.net>
2021-12-14 17:17:19 +0100

    Refactoring services

--- BLL/Services/LoginService.cs
+++ BLL/Services/LoginService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using DLL.Models;
+﻿using DLL.Models;
 using DLL.Repository;
 
 namespace BLL.Services {
@@ -9,17 +6,16 @@ namespace BLL.Services {
         private readonly LoginDataRepository _repository;
         public LoginService(LoginDataRepository repository) { _repository = repository; }
         public async Task<User?> Login(string login, string password) {
-            LoginData data;
+            List<LoginData> data;
             try {
                 data = (await _repository.FindByConditionAsync(x => login.Contains('@') == true
                                                                        ? x.Email == login &&
                                                                          x.Password == password
                                                                        : x.Login == login &&
-                                                                         x.Password == password)).ToList()
-                        .First();
+                                                                         x.Password == password)).ToList();
             }
             catch (Exception e) { return null; }
-            return data.User;
+            return data.Count == 0 ? null : data.First().User;
         }
     }
 }
