3535221137c9fea8e38917e2b503805bfa761090
Oleh <oleglovchuk@ukr.net>
2021-12-14 17:26:28 +0100

    Config services (DI)

--- UI/App.xaml.cs
+++ UI/App.xaml.cs
@@ -1,14 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using DLL.Models;
+using Microsoft.Extensions.DependencyInjection;
+using UI.Infrastructure;
+using UI.View;
+using UI.ViewModel;
 
 namespace UI {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application { }
+    public partial class App : Application {
+        public static IServiceProvider ServiceProvider = null!;
+        public App() {
+            var servicesCollection = new ServiceCollection();
+            ConfigServices(servicesCollection);
+            ServiceProvider = servicesCollection.BuildServiceProvider();
+        }
+        private static void ConfigServices(IServiceCollection serviceProvider) {
+            BllConfigureService.ConfigureService(serviceProvider
+                                               , ConfigurationManager.ConnectionStrings["testConnection"]
+                                                                     .ConnectionString);
+            serviceProvider.AddSingleton<ThemeViewModel>();
+            serviceProvider.AddSingleton<User>();
+            serviceProvider.AddTransient<LoginWindow>();
+            serviceProvider.AddTransient<MainWindow>();
+        }
+        private void App_OnStartup(object sender, StartupEventArgs e) {
+            // Initialize themes
+            var viewModelTheme = ServiceProvider.GetService<ThemeViewModel>();
+            ServiceProvider.GetService<MainWindow>()!.Show();
+        }
+    }
 }
