f3d34d22a48d17789042ea6f31ab13875f1635d9
Oleh <oleglovchuk@ukr.net>
2021-12-14 17:23:31 +0100

    Add ViewModel (Implementation of MVVM patern)

---
+++ UI/ViewModel/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace UI.ViewModel {
+    public class BaseViewModel : INotifyPropertyChanged, IDisposable {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public         void Dispose()   { this.OnDispose(); }
+        public virtual void OnDispose() { }
+    }
+}
---
+++ UI/ViewModel/ThemeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using UI.Enumeration;
+
+namespace UI.ViewModel {
+    public class ThemeViewModel : BaseViewModel{
+        public ThemeViewModel() {
+            ThemesList = new ObservableCollection<object>();
+            foreach (var i in Enum.GetValues(typeof(ThemeType))) { ThemesList.Add(i); }
+            Theme = ThemeType.Dark;
+            ChangeTheme(Theme);
+        }
+        private ThemeType _theme;
+        public ThemeType Theme {
+            get => _theme;
+            set {
+                _theme = value;
+                OnPropertyChanged(nameof(Theme));
+            }
+        }
+        public ObservableCollection<object> ThemesList { get; }
+        public void ChangeTheme(ThemeType theme) {
+            _theme = theme;
+            var name = theme.ToString();
+            var uri  = new Uri(@"View\Themes\" + name + ".xaml", UriKind.Relative);
+
+            var resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+    }
+}
