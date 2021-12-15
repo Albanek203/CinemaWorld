50e541a03d5fd8521c4d08e9741cd6d5a8b48394
Oleh <oleglovchuk@ukr.net>
2021-12-15 19:54:43 +0100

    Refactoring

--- DLL/Context/CinemaContext.cs
+++ DLL/Context/CinemaContext.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using DLL.Models;
+﻿using DLL.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DLL.Context {
     public class CinemaContext : DbContext {
-        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options) {
-            Database.EnsureCreated();
-        }
-
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options) { Database.EnsureCreated(); }
         public DbSet<User>        Users        { get; set; }
         public DbSet<LoginData>   LoginDatas   { get; set; }
         public DbSet<Person>      Persons      { get; set; }
@@ -16,7 +12,6 @@ namespace DLL.Context {
         public DbSet<Session>     Sessions     { get; set; }
         public DbSet<Film>        Films        { get; set; }
         public DbSet<Seat>        Seats        { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<LoginData>(LoginDataConfigure);
             modelBuilder.Entity<User>(UserConfigure);
@@ -26,10 +21,8 @@ namespace DLL.Context {
             modelBuilder.Entity<Film>(FilmConfigure);
             modelBuilder.Entity<Seat>(SeatConfigure);
         }
-
         private void UserConfigure(EntityTypeBuilder<User> builder) {
             builder.Property(x => x.Role).IsRequired();
-
             builder.HasOne(x => x.Person)
                    .WithOne(x => x.User)
                    .HasForeignKey<User>(x => x.PersonId)
@@ -40,49 +33,40 @@ namespace DLL.Context {
                    .IsRequired();
             builder.HasMany(x => x.ActionsData)
                    .WithOne(x => x.User);
-
             builder.HasData(new User {
-                Id = 1,
-                PersonId = 1,
-                LoginDataId = 1,
-                Role = 0,
-            });
+                                    Id = 1
+                                  , PersonId = 1
+                                  , LoginDataId = 1
+                                  , Role = 0
+                                   ,
+                            });
         }
-
         private void LoginDataConfigure(EntityTypeBuilder<LoginData> builder) {
             builder.Property(x => x.Email).IsRequired();
             builder.Property(x => x.Login).IsRequired();
             builder.Property(x => x.Password).IsRequired();
-
             builder.HasOne(x => x.User)
                    .WithOne(x => x.LoginData)
                    .IsRequired();
-
             builder.HasData(new LoginData {
-                Id = 1,
-                Email = "albanekpiss@gmail.com",
-                Login = "funnybored24",
-                Password = "strongpassbymy24"
+                    Id = 1, Email = "albanekpiss@gmail.com", Login = "funnybored24", Password = "strongpassbymy24"
             });
         }
-
         private void PersonConfigure(EntityTypeBuilder<Person> builder) {
             builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
             builder.Property(x => x.Surname).HasMaxLength(50).IsRequired();
             builder.Property(x => x.Salary).IsRequired();
             builder.Property(x => x.BirthDate).IsRequired();
             builder.Property(x => x.EmployeeDate).IsRequired();
-
             builder.HasData(new Person {
-                Id = 1,
-                Name = "Oleh",
-                Surname = "Border",
-                Salary = 4200,
-                BirthDate = new DateTime(2002, 7, 24),
-                EmployeeDate = new DateTime(2012, 4, 12)
-            });
+                                    Id = 1
+                                  , Name = "Oleh"
+                                  , Surname = "Border"
+                                  , Salary = 4200
+                                  , BirthDate = new DateTime(2002, 7, 24)
+                                  , EmployeeDate = new DateTime(2012, 4, 12)
+                            });
         }
-
         private void ActionsDatasConfigure(EntityTypeBuilder<ActionsData> builder) {
             builder.HasOne(x => x.User)
                    .WithMany(x => x.ActionsData)
@@ -93,11 +77,9 @@ namespace DLL.Context {
             builder.Property(x => x.Cost).IsRequired();
             builder.Property(x => x.Time).IsRequired();
         }
-
         private void SessionConfigure(EntityTypeBuilder<Session> builder) {
             builder.Property(x => x.HallNumber).IsRequired();
             builder.Property(x => x.DateSession).IsRequired();
-
             builder.HasOne(x => x.Film)
                    .WithOne(x => x.Session)
                    .HasForeignKey<Session>(x => x.FilmId)
@@ -106,7 +88,6 @@ namespace DLL.Context {
                    .WithOne(x => x.Session)
                    .IsRequired();
         }
-
         private void FilmConfigure(EntityTypeBuilder<Film> builder) {
             builder.Property(x => x.Name).IsRequired();
             builder.Property(x => x.Genre).IsRequired();
@@ -115,18 +96,15 @@ namespace DLL.Context {
             builder.Property(x => x.Is3D).IsRequired();
             builder.Property(x => x.Rating).IsRequired();
             builder.Property(x => x.Price).IsRequired();
-
             builder.HasOne(x => x.Session)
                    .WithOne(x => x.Film)
                    .HasForeignKey<Film>(x => x.SessionId)
                    .IsRequired();
         }
-
         private void SeatConfigure(EntityTypeBuilder<Seat> builder) {
             builder.Property(x => x.Number).IsRequired();
             builder.Property(x => x.Price).IsRequired();
             builder.Property(x => x.Status).IsRequired();
-
             builder.HasOne(x => x.Session)
                    .WithMany(x => x.Seats)
                    .IsRequired();
