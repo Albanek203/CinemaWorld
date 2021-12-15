0fbb5fd8a25e421f75aee6030b70ac7465a85db7
Oleh <oleglovchuk@ukr.net>
2021-12-15 18:07:05 +0100

    Added FilmService

---
+++ BLL/Services/FilmService.cs
@@ -0,0 +1,10 @@
+﻿using DLL.Models;
+using DLL.Repository;
+
+namespace BLL.Services;
+
+public class FilmService {
+    private readonly FilmRepository _filmRepository;
+    public FilmService(FilmRepository filmRepository) { _filmRepository = filmRepository; }
+    public async Task<IReadOnlyCollection<Film>> GetAllFilmsAsync() => await _filmRepository.GetAllAsync();
+}
