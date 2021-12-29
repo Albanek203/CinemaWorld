20017e1bf6eb8e2dbe9fdea37f22e13fc283d857
Albanek203 <oleglovchuk@ukr.net>
2021-12-29 14:34:24 +0200

    Added function auto authorisation / added some viewmodel & view

--- UI/App.xaml.cs
+++ UI/App.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
 using System.Windows;
 using DLL.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using UI.Infrastructure;
 using UI.View;
+using UI.View.Pages;
 using UI.ViewModel;
 
 namespace UI {
@@ -12,6 +15,7 @@ namespace UI {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        private readonly string _saveUserDataPath = Directory.GetCurrentDirectory() + "\\saveLU.data";
         public static IServiceProvider ServiceProvider = null!;
         public App() {
             var servicesCollection = new ServiceCollection();
@@ -21,16 +25,38 @@ namespace UI {
         private static void ConfigServices(IServiceCollection serviceProvider) {
             BllConfigureService.ConfigureService(serviceProvider
                                                , ConfigurationManager.ConnectionStrings["testConnection"]
-                                                                     .ConnectionString);
-            serviceProvider.AddSingleton<ThemeViewModel>();
+                                                     .ConnectionString);
+            // Models
             serviceProvider.AddSingleton<User>();
-            serviceProvider.AddTransient<LoginWindow>();
-            serviceProvider.AddTransient<MainWindow>();
+
+            // View models
+            serviceProvider.AddSingleton<ThemeViewModel>();
+            serviceProvider.AddTransient<SessionLibraryViewModel>();
+            serviceProvider.AddTransient<LoginViewModel>();
+            serviceProvider.AddTransient<MainViewModel>();
+
+            // Pages
+            serviceProvider.AddTransient<FilmLibraryView>();
+
+            // Views
+            serviceProvider.AddTransient<LoginView>();
+            serviceProvider.AddTransient<MainView>();
         }
         private void App_OnStartup(object sender, StartupEventArgs e) {
             // Initialize themes
             var viewModelTheme = ServiceProvider.GetService<ThemeViewModel>();
-            ServiceProvider.GetService<LoginWindow>()!.Show();
+            if (File.Exists(_saveUserDataPath)) {
+                var user    = JsonConvert.DeserializeObject<User>(File.ReadAllText(_saveUserDataPath));
+                var curUser = ServiceProvider.GetService<User>();
+                curUser!.Id         = user!.Id;
+                curUser.Person      = user.Person;
+                curUser.Role        = user.Role;
+                curUser.ActionsData = user.ActionsData;
+                curUser.LoginData   = user.LoginData;
+                ServiceProvider.GetService<MainView>()!.Show();
+            }
+            else
+                ServiceProvider.GetService<LoginView>()!.Show();
         }
     }
 }
