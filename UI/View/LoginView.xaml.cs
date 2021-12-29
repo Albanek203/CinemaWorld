540556c5ff9cc4ed57870a0681c8ec1fde31d0c1
Albanek203 <oleglovchuk@ukr.net>
2021-12-29 14:19:31 +0200

    Added localization / refactoring

---
+++ UI/View/LoginView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using UI.ViewModel;
+
+namespace UI.View {
+    public partial class LoginView : Window {
+        public LoginView(LoginViewModel viewModel) {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+        private void UILogin_OnMouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+    }
+}
