26cc038bacb443f7814b41c7c726a2bc24644baf
Albanek203 <oleglovchuk@ukr.net>
2021-12-29 14:09:48 +0200

    Added MainView

---
+++ UI/View/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using UI.ViewModel;
+
+namespace UI.View {
+    public partial class MainView : Window {
+        public MainView(MainViewModel viewModel) {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+        private void UIMain_OnMouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+    }
+}
