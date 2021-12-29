7d70b860d1057f2aac13bfba707f77e3021ba12b
Albanek203 <oleglovchuk@ukr.net>
2021-12-29 14:32:44 +0200

    Added classes for converting values

---
+++ UI/Converters/EnumConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Data;
+using UI.Enumeration;
+
+namespace UI.Converters {
+    public class EnumConverter : IValueConverter {
+        public object Convert(object                           value
+                            , Type                             targetType
+                            , object                           parameter
+                            , System.Globalization.CultureInfo culture) {
+            return (GenreType)value;
+        }
+        public object ConvertBack(object                           value
+                                , Type                             targetType
+                                , object                           parameter
+                                , System.Globalization.CultureInfo culture) {
+            return null;
+        }
+    }
+}
