a9f1c585589d6995f36eb1c0b84134b0fd112c34
Albanek203 <oleglovchuk@ukr.net>
2021-12-29 14:10:43 +0200

    Added MainViewModel (control MainView)

---
+++ UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using UI.Infrastructure;
+using UI.View.Pages;
+
+namespace UI.ViewModel {
+    public class MainViewModel : WindowViewModel {
+        private readonly FilmLibraryView _filmLibraryView;
+        public MainViewModel(FilmLibraryView filmLibraryView) { _filmLibraryView = filmLibraryView; }
+        private Page _currentPage;
+        public Page CurrentPage {
+            get => _currentPage;
+            set {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
+        private RelayCommand _showFilmLibrary;
+        public ICommand ShowFilmLibrary =>
+            _showFilmLibrary ??= new RelayCommand(ExecuteShowFilmLibrary, CanExecuteShowFilmLibrary);
+        private void ExecuteShowFilmLibrary(object    obj) { CurrentPage = _filmLibraryView; }
+        private bool CanExecuteShowFilmLibrary(object obj) => true;
+    }
+}
