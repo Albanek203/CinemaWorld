b622e94463ae780e161d1ac08a41c6bb94d42586
Albanek203 <oleglovchuk@ukr.net>
2022-01-06 11:56:04 +0200

    Added thenInclude for seats

--- DLL/Repository/FilmRepository.cs
+++ DLL/Repository/FilmRepository.cs
@@ -8,15 +8,14 @@ namespace DLL.Repository {
     public class FilmRepository : BaseRepository<Film> {
         public FilmRepository(CinemaContext context) : base(context) { }
         public override async Task<IReadOnlyCollection<Film>> GetAllAsync() {
-            return await Entities.Include(session => session.Session)
+            return await Entities.Include(session => session.Sessions)!
                                  .ThenInclude(session => session!.Seats)
                                  .ToListAsync()
                                  .ConfigureAwait(false);
         }
         public override async Task<IReadOnlyCollection<Film>> FindByConditionAsync(
                 Expression<Func<Film, bool>> predicate) {
-            return await Entities.Where(predicate)
-                                 .Include(session => session.Session)
+            return await Entities.Where(predicate).Include(session => session.Sessions)!
                                  .ThenInclude(session => session!.Seats)
                                  .ToListAsync()
                                  .ConfigureAwait(false);
