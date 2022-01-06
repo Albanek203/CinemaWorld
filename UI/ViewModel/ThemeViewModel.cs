883a732dc13cd8b5195316dae2361ce8abc1b0cd
Albanek203 <oleglovchuk@ukr.net>
2022-01-06 12:01:45 +0200

    Refactoring

--- UI/ViewModel/ThemeViewModel.cs
+++ UI/ViewModel/ThemeViewModel.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using UI.Enumeration;
 
 namespace UI.ViewModel {
-    public class ThemeViewModel : BaseViewModel{
+    public class ThemeViewModel : BaseViewModel {
         public ThemeViewModel() {
             ThemesList = new ObservableCollection<object>();
             foreach (var i in Enum.GetValues(typeof(ThemeType))) { ThemesList.Add(i); }
@@ -22,9 +22,8 @@ namespace UI.ViewModel {
         public ObservableCollection<object> ThemesList { get; }
         public void ChangeTheme(ThemeType theme) {
             _theme = theme;
-            var name = theme.ToString();
-            var uri  = new Uri(@"View\Themes\" + name + ".xaml", UriKind.Relative);
-
+            var name         = theme.ToString();
+            var uri          = new Uri(@"View\Themes\" + name + ".xaml", UriKind.Relative);
             var resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
             Application.Current.Resources.Clear();
             Application.Current.Resources.MergedDictionaries.Add(resourceDict);
