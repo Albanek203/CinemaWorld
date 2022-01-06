de3e8a057e1ad09c5a6c02b604e09045c0b413d9
Albanek203 <oleglovchuk@ukr.net>
2022-01-06 13:08:02 +0200

    Change CurrentWindow to protected

--- UI/ViewModel/WindowViewModel.cs
+++ UI/ViewModel/WindowViewModel.cs
@@ -4,7 +4,7 @@ using UI.Infrastructure;
 
 namespace UI.ViewModel {
     public abstract class WindowViewModel : BaseViewModel {
-        private Window? _currentWindow;
+        protected Window? CurrentWindow;
         private RelayCommand _closeWindow;
         public ICommand CloseWindow => _closeWindow ??= new RelayCommand(ExecuteCloseWindow, CanExecuteCloseWindow);
         private void ExecuteCloseWindow(object    obj) { Application.Current.Shutdown(); }
@@ -14,7 +14,7 @@ namespace UI.ViewModel {
             _collapseWindow ??= new RelayCommand(ExecuteCollapseWindow, CanExecuteCollapseWindow);
         private void ExecuteCollapseWindow(object obj) { (obj as Window)!.WindowState = WindowState.Minimized; }
         private bool CanExecuteCollapseWindow(object obj) {
-            _currentWindow ??= obj as Window;
+            CurrentWindow ??= obj as Window;
             return true;
         }
     }
