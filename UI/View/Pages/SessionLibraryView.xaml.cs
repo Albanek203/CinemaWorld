3705dba349735481fdd0e1bec87e8afae5d4ad0a
Albanek203 <oleglovchuk@ukr.net>
2022-01-06 12:05:30 +0200

    SessionLibraryView to show all film and session / sell & reservation ticket

---
+++ UI/View/Pages/SessionLibraryView.xaml.cs
@@ -0,0 +1,10 @@
+﻿using UI.ViewModel;
+
+namespace UI.View.Pages {
+    public partial class SessionLibraryView {
+        public SessionLibraryView(SessionLibraryViewModel viewModel) {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
